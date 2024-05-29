474583894c0e9149b6391d6886e4fa3d99d64dc4
Saugat Adhikari <asaugat04@gmail.com>
2024-05-29 13:45:11 +0545

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar w-full">
+      <div className="container">
+        <a className="navbar-brand" href="/">
+          <img src="https://servicemarket.com/dist/images/servicemarket-by-smiles-main-logo.svg" />
+        </a>
+      </div>
+    </nav>
+  );
+}
